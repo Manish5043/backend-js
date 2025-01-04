6aaf883fa7161be6f22484a86aff6ac90463808e
Manish5043 <manishsoni8121@gmail.com>
2025-01-04 21:28:53 +0530

    util code diss

---
+++ src/utils/ApiError.js
@@ -0,0 +1,31 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "sometinh went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+
+        if(stack) {
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+
+    // this.data = null is a common practice 
+    // to indicate that the data property is part of t
+    // he object structure but currently does not ho
+    // ld any value. It improves clarity, consistency, a
+    // nd helps avoid issues with uninitialized 
+    // properties.
+}
+
+export {ApiError}
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,6 +1,10 @@
-// const asyncHandler = () => {
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+        
+    }
 
-// }
+}
 
 export {asyncHandler};
 
@@ -10,5 +14,16 @@ export {asyncHandler};
 // const asyncHandler = (func)=> ()=> {}
 // const asyncHandler = (func) => async() => {}
 
+// hum ek wrapper function bana rahe hai jo ki baaar baaar use hoga
 
-const asyncHandler = (fn) => () => {}    
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }    
