da1109d30f1a26a577e61b6238a492a781763ae3
Manish5043 <manishsoni8121@gmail.com>
2025-01-07 14:42:58 +0530

    login method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,27 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+// asynhandler mainly web request ke liye hote ha to uska bhi dhyan rakhein
+
+
+
+const generateaccessandrefreshtokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating refresh and access token")
+    }
+}
 
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from froentend
@@ -17,7 +38,7 @@ const registerUser = asyncHandler( async (req,res) => {
     //return res
 
     const {fullname, email, username,password} = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     // if(fullname === "") {
     //     throw new ApiError(400, "fullname is required")
@@ -36,6 +57,8 @@ const registerUser = asyncHandler( async (req,res) => {
     if(existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
+
+    // actual mein muter req ke saath files ka access de deta hai
     // code functionalities written within multer properties
     const avatarlocalpath = req.files?.avatar[0]?.path;
     //const coveriamgelocalpath = req.files?.coverImage[0]?.path;
@@ -82,4 +105,70 @@ const registerUser = asyncHandler( async (req,res) => {
 })
 
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and refresh token
+    // send token as cookies
+
+    const {email, username, password} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400, "username or email is required")    
+    }
+
+    const user = await User.findOne({
+        // these are mainly mongodb operators
+        $or: [{username}, {email}]
+    })
+// findone or updateone ye mongodb ke mongoose ke methods h jo humne capital mein user object banaya h uske to uska dhyan rakhe
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+
+    const {accessToken, refreshToken}= await generateaccessandrefreshtokens(user._id)
+
+
+    const loggedInuser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInuser,accessToken,refreshToken
+            },
+            "User logged in Successfully"
+        )
+        // actually mein jo humne upar likha ha usko destructure aise h ki pahle statuscode diya gya uske baat data jo humne define kiya tha apirepsone wali file mein aur end me message dediya hai humne
+    )
+
+
+})
+
+const logoutUser = asyncHandler(async(req, res)=> {
+    
+})
+
+
+export {registerUser,
+    loginUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
 router.route("/register").post(
+    // ye neeche ki functionality mein humne middleware lagya hana ki 3rd party use kiya h 
     upload.fields([
         {
            name: "avatar",
