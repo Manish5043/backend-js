6e14b0a0d7787306a15bbef787a00297eea4a4c3
Manish5043 <manishsoni8121@gmail.com>
2025-01-08 11:46:13 +0530

    exclamation mark added to incominguser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -189,7 +189,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
     // ko body wala req h wo mainly mobile apps ke liye h
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unathorized request")
     }
 
